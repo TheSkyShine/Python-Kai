762ce947447f76589f6f9a02832e011e70f04616
TheSkyShine <77767794+TheSkyShine@users.noreply.github.com>
2021-04-08 09:18:40 -0600

    Update Live_Plotter.py

--- Live_Plotter.py
+++ Live_Plotter.py
@@ -1,39 +1,30 @@
-from itertools import count
 import pandas as pd
 import matplotlib.pyplot as plt
 from matplotlib.animation import FuncAnimation
 
-x_value = 0
-total_1 = 1000
-total_2 = 1000
-
-fieldnames = ["x_value", "total_1", "total_2"]
-
-
+# Changes style of plot
 plt.style.use('fivethirtyeight')
 
-x_vals = []
-y_vals = []
-
-index = count()
-
-
+# Creates function to be called by FuncAnimation class. Reads .csv and seperates data into lists
 def animate(i):
     data = pd.read_csv('data.csv')
     x = data['x_value']
     y1 = data['total_1']
     y2 = data['total_2']
 
+# clears axes. If not included, old plot will remain when new one gets added on top
     plt.cla()
 
+# Plots the datasets
     plt.plot(x, y1, label='Channel 1')
     plt.plot(x, y2, label='Channel 2')
 
     plt.legend(loc='upper left')
     plt.tight_layout()
 
-
-ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+# FuncAnimation class is called. plt.gcf() is get current figure, animate is the function to be called,
+# and interval is how often to call said function
+ani = FuncAnimation(plt.gcf(), animate, interval=500)
 
 plt.tight_layout()
 plt.show()
